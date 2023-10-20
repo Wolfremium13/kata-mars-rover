1cb1efa397fabf0145e1456e02e1ca81f3ab771f
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-20 11:32:04 +0100

    🔥 cleaup repository

--- src/core/rover/application/orientation.ts
+++
@@ -1,8 +0,0 @@
-import { Position } from "../domain/position";
-
-export interface Direction {
-	what_is_left(): Direction;
-	what_is_right(): Direction;
-    what_is_forward(position: Position): Position;
-    what_is_backward(position: Position): Position;
-}
