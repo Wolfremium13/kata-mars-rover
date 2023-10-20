115dd894bd8767740a0c4299cb0ff320f01ffba4
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-20 15:14:35 +0100

    ❌ be able to move forward twice

--- src/tests/rover.test.ts
+++ src/tests/rover.test.ts
@@ -18,5 +18,17 @@ describe('Rover should', () => {
                 new Rover(startDirection, new Coordinate(0, 1), mars)
             );
 		});
+        it('forward twice', () => {
+            const startDirection = Direction.NORTH;
+            const startCoordinate = new Coordinate(0, 0);
+            const mars = new Planet(2, 2);
+            const rover = new Rover(startDirection, startCoordinate, mars);
+
+            rover.executeCommands(['F', 'F']);
+
+            expect(rover).toMatchObject(
+                new Rover(startDirection, new Coordinate(0, 2), mars)
+            );
+        });
 	});
 });
