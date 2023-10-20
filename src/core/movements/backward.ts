e97841f697d172b38a4b94ab54a5a1facf6d143b
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-20 17:16:34 +0100

    ✅ added all backward movement posibilites

--- src/core/movements/backward.ts
+++ src/core/movements/backward.ts
@@ -4,8 +4,30 @@ import { Planet } from '../planet';
 import { Movement } from './movement';
 
 export class MoveBackward implements Movement {
-	constructor(direction: Direction) {}
+	constructor(private readonly direction: Direction) {}
 	move(currentPosition: Coordinate, planet: Planet): Coordinate {
+		const mapDirectionToMovement = {
+			N: () => this.moveNorth(currentPosition),
+			S: () => this.moveSouth(currentPosition),
+			E: () => this.moveEast(currentPosition),
+			W: () => this.moveWest(currentPosition),
+		};
+		return mapDirectionToMovement[this.direction.getCode()]();
+	}
+
+	private moveNorth(currentPosition: Coordinate): Coordinate {
 		return new Coordinate(currentPosition.x, currentPosition.y - 1);
+	}
+
+	private moveSouth(currentPosition: Coordinate): Coordinate {
+		return new Coordinate(currentPosition.x, currentPosition.y + 1);
+	}
+
+	private moveEast(currentPosition: Coordinate): Coordinate {
+		return new Coordinate(currentPosition.x - 1, currentPosition.y);
+	}
+
+	private moveWest(currentPosition: Coordinate): Coordinate {
+		return new Coordinate(currentPosition.x + 1, currentPosition.y);
 	}
 }
--- src/tests/movements.test.ts
+++ src/tests/movements.test.ts
@@ -3,12 +3,16 @@ import { North } from '../core/directions/north';
 import { Coordinate } from '../core/coordinate';
 import { MoveForward } from '../core/movements/forward';
 import { Planet } from '../core/planet';
+import { South } from '../core/directions/south';
+import { West } from '../core/directions/west';
+import { East } from '../core/directions/east';
+import { MoveBackward } from '../core/movements/backward';
 
 describe('Movements should', () => {
 	describe('be able to move', () => {
 		const planet = new Planet(10, 10);
 		describe('forward', () => {
-            const startPosition = new Coordinate(5, 5);
+			const startPosition = new Coordinate(5, 5);
 			it('when facing north', () => {
 				const direction = new North();
 				const movement = new MoveForward(direction);
@@ -17,30 +21,64 @@ describe('Movements should', () => {
 				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
 			});
 
-            it('when facing south', () => {
-                const direction = new North().whatIsLeft().whatIsLeft();
-                const movement = new MoveForward(direction);
+			it('when facing south', () => {
+				const direction = new South();
+				const movement = new MoveForward(direction);
 
-                const expectedCoordinate = new Coordinate(5, 4);
-                expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
-            });
+				const expectedCoordinate = new Coordinate(5, 4);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
 
-            it('when facing east', () => {
-                const direction = new North().whatIsRight();
-                const movement = new MoveForward(direction);
+			it('when facing east', () => {
+				const direction = new East();
+				const movement = new MoveForward(direction);
 
-                const expectedCoordinate = new Coordinate(6, 5);
-                expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
-            });
+				const expectedCoordinate = new Coordinate(6, 5);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
 
-            it('when facing west', () => {
-                const direction = new North().whatIsLeft();
-                const movement = new MoveForward(direction);
+			it('when facing west', () => {
+				const direction = new West();
+				const movement = new MoveForward(direction);
 
-                const expectedCoordinate = new Coordinate(4, 5);
-                expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
-            });
+				const expectedCoordinate = new Coordinate(4, 5);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
 		});
 
+		describe('backward', () => {
+			const startPosition = new Coordinate(5, 5);
+			it('when facing north', () => {
+				const direction = new North();
+				const movement = new MoveBackward(direction);
+
+				const expectedCoordinate = new Coordinate(5, 4);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
+
+			it('when facing south', () => {
+				const direction = new South();
+				const movement = new MoveBackward(direction);
+
+				const expectedCoordinate = new Coordinate(5, 6);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
+
+			it('when facing east', () => {
+				const direction = new East();
+				const movement = new MoveBackward(direction);
+
+				const expectedCoordinate = new Coordinate(4, 5);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
+
+			it('when facing west', () => {
+				const direction = new West();
+				const movement = new MoveBackward(direction);
+
+				const expectedCoordinate = new Coordinate(6, 5);
+				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
+		});
 	});
 });
