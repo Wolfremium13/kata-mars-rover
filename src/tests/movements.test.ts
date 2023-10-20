5954218c3dcc7ed5db76c7ca623d7e241a37f651
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-20 18:47:53 +0100

    ✅ added fastcheck for movement random checks

--- src/tests/movements.test.ts
+++ src/tests/movements.test.ts
@@ -7,6 +7,7 @@ import { South } from '../core/directions/south';
 import { West } from '../core/directions/west';
 import { East } from '../core/directions/east';
 import { MoveBackward } from '../core/movements/backward';
+import * as fc from 'fast-check';
 
 describe('Movements should', () => {
 	describe('be able to move', () => {
@@ -78,6 +79,36 @@ describe('Movements should', () => {
 
 				const expectedCoordinate = new Coordinate(6, 5);
 				expect(movement.move(startPosition, planet)).toStrictEqual(expectedCoordinate);
+			});
+		});
+
+		describe('on a random combination', () => {
+			const planet = new Planet(10, 10);
+    		const startPosition = new Coordinate(5, 5);
+			const directionsArbitrary = fc.oneof(
+				fc.constant(new North()),
+				fc.constant(new South()),
+				fc.constant(new East()),
+				fc.constant(new West())
+			);
+		
+			it('be able to move in any direction and movement within planet boundaries', () => {
+				fc.assert(
+					fc.property(directionsArbitrary, (direction) => {
+						const forwardMovement = new MoveForward(direction);
+						const backwardMovement = new MoveBackward(direction);
+		
+						const forwardResult = forwardMovement.move(startPosition, planet);
+						const backwardResult = backwardMovement.move(startPosition, planet);
+		
+						expect(isWithinPlanetBoundaries(forwardResult, planet)).toBeTruthy();
+                		expect(isWithinPlanetBoundaries(backwardResult, planet)).toBeTruthy();
+					})
+				);
+				function isWithinPlanetBoundaries(coordinate, planet) {
+					return coordinate.x >= 0 && coordinate.x <= planet.width &&
+						   coordinate.y >= 0 && coordinate.y <= planet.height;
+				}
 			});
 		});
 	});
