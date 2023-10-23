4725b95c8ecd61887277339d5d0b1b9721fc8b7c
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-23 18:55:59 +0100

    💡 add a decision comment

--- src/core/directions/direction.ts
+++ src/core/directions/direction.ts
@@ -1,5 +1,6 @@
 export type DirectionCode = 'N' | 'E' | 'S' | 'W';
 export interface Direction {
+    // Makes sense to move this logic into Movements??
     whatIsRight(): Direction;
     whatIsLeft(): Direction;
     getCode(): DirectionCode;
